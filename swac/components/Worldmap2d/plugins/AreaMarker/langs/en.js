35d8ed7b9c3e018cb0edac12ef6300202c2e2082
flori <flori@DESKTOP-UBGV4OD.dhcp.fh-bielefeld.de>
2024-10-18 12:39:02 +0200

    Fixed: Worldmap2d: AreaMarker: Wrong ids in language files

--- swac/components/Worldmap2d/plugins/AreaMarker/langs/en.js
+++ swac/components/Worldmap2d/plugins/AreaMarker/langs/en.js
@@ -1,19 +1,4 @@
-var Worldmap2d_CreateMeasurementModal_en = {
-  title: 'Create Measurement Point',
-  name: 'Name of the Measurement Point',
-  name_placeholder: 'Type new name',
-  status: 'Status',
-  status_true: 'Measuring pending',
-  status_false: 'Measuring completed',
-  lat: 'Latitude',
-  lng: 'Longitude',
-  collection: 'Tablename for Measurement Values',
-  collection_placeholder: 'Type new table name',
-  type: 'Measurement Point Type',
-  description: 'Description',
-  description_placeholder: 'Type description',
-  buttonCancel: 'Cancel',
-  buttonCreate: 'Create',
-}
+var Worldmap2d_AreaMarker_en = {
+};
 
-export default Worldmap2d_CreateMeasurementModal_en
+export default Worldmap2d_AreaMarker_en;
