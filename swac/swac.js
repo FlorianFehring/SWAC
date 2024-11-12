b9b28e6b310dceb02933a47394c5850937014ea3
flori <flori@DESKTOP-UBGV4OD.dhcp.fh-bielefeld.de>
2024-11-12 16:12:45 +0100

    Changed: Updated version information

--- swac/swac.js
+++ swac/swac.js
@@ -12,7 +12,7 @@ import Model from './Model.js';
 var SWAC = {
     desc: {
         name: 'core',
-        version: '18.10.2024'
+        version: '12.11.2024'
     },
     config: {},
     msgs: new Map(),
