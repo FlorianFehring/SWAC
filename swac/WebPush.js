2a338ca686ca325f8cf186ce900f72647fa98580
Alexander Nungesser <Alexander.Nungesser@wago.com>
2025-10-30 16:47:16 +0100

    refactor(general): webpush, delete one line from swac

--- swac/WebPush.js
+++ swac/WebPush.js
@@ -2,55 +2,42 @@ import SWAC from "./swac.js"
 
 export default class WebPush {
 
-    informUser() {
-        
-    }
-
     async subscribe() {
-        // Addition for push notifications
         if (SWAC.config.progressive.supportpush) {
-            const rawkey = await this.getKey();
-            console.log("Key: ", rawkey)
-            const subscription = await SWAC.swRegistration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: SWAC.urlBase64ToUint8Array(rawkey)
-            });
+            const subscription = await navigator.serviceWorker.ready.then(async registration =>
+                registration.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: SWAC.urlBase64ToUint8Array(await this.getKey())
+                })
+            );
 
             var key = subscription.getKey ? subscription.getKey('p256dh') : '';
             var auth = subscription.getKey ? subscription.getKey('auth') : '';
 
-            const body = JSON.stringify({
-                            endpoint: subscription.endpoint,
-                            // Take byte[] and turn it into a base64 encoded string suitable for
-                            // POSTing to a server over HTTP
-                            key: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '',
-                            auth: auth ? btoa(String.fromCharCode.apply(null, new Uint8Array(auth))) : ''
-                        });
-
-            fetch('https://localhost:8181/WebPush/smarttemplate/push/subscribe', {
+            return await fetch('https://localhost:8181/WebPush/smarttemplate/push/subscribe', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: body
-            }).then(response => {
-                if (!response.ok) {
-                throw new Error('HTTP-Fehler: ' + response.status);
-                }
-                return response.text(); // JSON in JS-Objekt umwandeln
-            }).then(data => {
-                console.log('Antwortobjekt:', data);
-            });
-
-            console.log('Push subscription erfolgreich registriert:', body);
+                body: JSON.stringify({
+                    endpoint: subscription.endpoint,
+                    key: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '',
+                    auth: auth ? btoa(String.fromCharCode.apply(null, new Uint8Array(auth))) : ''
+                })
+            }).then(response => response.json());
         }
     }
 
     async getKey() {
-        let base64;
-        const response = await fetch('https://localhost:8181/WebPush/smarttemplate/push/key', {
+        return await fetch('https://localhost:8181/WebPush/smarttemplate/push/key', {
             method: 'GET',
-            headers: {}
-        });
-        return response.text();
+            headers: { 'Content-Type': 'application/json' }
+        }).then(response => response.json()).then(data => data.key);
+
     }
 
+    async send() {
+        await fetch('https://localhost:8181/WebPush/smarttemplate/push/send', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
 }
